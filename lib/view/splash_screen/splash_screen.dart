7d691fb8c331484b7d25cc1209c89a779ed07bba
Yaseeeeeen <96672031+yaseeeeeeeeeeen@users.noreply.github.com>
2024-01-24 16:49:51 +0530

    upcoming-vehicle-list-issue-fixed

--- lib/view/splash_screen/splash_screen.dart
+++ lib/view/splash_screen/splash_screen.dart
@@ -78,8 +78,8 @@ class _SplashScreenState extends State<SplashScreen> {
       locator<VehicleCheckBloc>().allBookedVehicles = datas;
       //////////////////// DIVIED BOOKED AND NOT BOOKED////////////////////////////////
       final bookedonly = datas.where((element) {
-        final endDate = DateTime.parse(element.endDate);
-        return endDate.isBefore(currentDate) && element.status == "Booked";
+        final startDate = DateTime.parse(element.startDate);
+        return startDate.isBefore(currentDate) && element.status == "Booked";
       }).toList();
       locator<VehicleCheckBloc>().bookedVehicles = bookedonly;
       ////////////////// ACTIVE VEHICLE SORTING/////////////////////////////////
@@ -87,7 +87,8 @@ class _SplashScreenState extends State<SplashScreen> {
         final startDate = DateTime.parse(element.startDate);
         final endDate = DateTime.parse(element.endDate);
         return startDate.isBefore(currentDate) &&
-                endDate.isAfter(currentDate) && element.status == "Booked";
+            endDate.isAfter(currentDate) &&
+            element.status == "Booked";
       }).toList();
     });
     Navigator.of(context).pushAndRemoveUntil(
